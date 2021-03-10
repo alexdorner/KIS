7caa69080843583fc1ec719929d2e5931bf147aa
Alexandra Dorner <56341637+alexdorner@users.noreply.github.com>
2021-03-10 18:21:22 +0100

    final change json

--- kis/kis/doctype/patient_appointment/patient_appointment.js
+++ kis/kis/doctype/patient_appointment/patient_appointment.js
@@ -1,9 +1,60 @@
 // Copyright (c) 2016, ESS LLP and contributors
 // For license information, please see license.txt
-//frappe.provide('erpnext.queries');
+
 frappe.ui.form.on('Patient Appointment', {
 
+    setup: function (frm) {
+        frm.custom_make_buttons = {
+            'Patient': 'Patient'
 
+        };
+    },
+
+    onload: function (frm) {
+        if (frm.is_new()) {
+            frm.set_value('appointment_time', null);
+            frm.disable_save();
+        }
+    }
+
+},
+
+	function show_availability() {
+		let ID = '';
+		let d = new frappe.ui.Dialog({
+			title: __('Available Appointment'),
+			fields: [
+				{ fieldtype: 'Link', options: 'Medical Department', reqd: 1, fieldname: 'department', label: 'Medical Department'},
+				{ fieldtype: 'Column Break'},
+				{ fieldtype: 'Link', options: 'Patient', reqd: 1, fieldname: 'ID', label: 'Patient'},
+				{ fieldtype: 'Column Break'},
+				{ fieldtype: 'Date', reqd: 1, fieldname: 'appointment_date', label: 'Date'},
+
+
+			],
+			primary_action_label: __('Book'),
+			primary_action: function() {
+				frm.set_value('appointment_time', appointment_time);
+				if (!frm.doc.duration) {
+					frm.set_value('duration', duration);
+				}
+				frm.set_value('company', d.get_value('company'));
+				frm.set_value('department', d.get_value('department'));
+				frm.set_value('appointment_date', d.get_value('appointment_date'));
+				if (service_unit) {
+					frm.set_value('service_unit', service_unit);
+				}
+				d.hide();
+				frm.enable_save();
+				frm.save();
+			}
+		});
+
+		d.set_values({
+			'department': frm.doc.department,
+			'patient': frm.doc.patient,
+			'appointment_date': frm.doc.appointment_date
+		});
 
 });
 
--- kis/kis/doctype/patient_appointment/patient_appointment.py
+++ kis/kis/doctype/patient_appointment/patient_appointment.py
@@ -44,12 +44,41 @@ def cancel_appointment(appointment_id):
 	appointment = frappe.get_doc('Patient Appointment', appointment_id)
 	return appointment
 
-	msg = _('Appointment Cancelled.')
+	msg = _('Appointment {0} Cancelled.')
 
 	frappe.msgprint(msg)
 
+@frappe.whitelist()
+def get_availability_data(ID,appointment):
+
+	appointment = frappe.get_doc('Patient Appointment', ID)
+
+	if ID.null == 0:
+		return appointment
+	else:
+		return None
 
 
+def validate_overlaps(self):
 
+	overlaps = frappe.db.sql("""
+	select
+		appointment, ID, appointment_time, duration
+	from
+		`tabPatient Appointment`
+	where
+		appointment_date=%s and Appointment!=%s and ID=%s) and
+		(appointment_time=%s))
+	""", (self.appointment_date, self.Appointment, self.ID,
+		  self.appointment_time))
+
+	if overlaps:
+		overlapping_details = _('Appointment overlaps with ')
+		overlapping_details += "<b><a href='/app/Form/Patient Appointment/{0}'>{0}</a></b><br>".format(
+			overlaps[0][0])
+		overlapping_details += _(
+			'{0} has appointment scheduled at {2} having {3} minute(s) duration.').format(
+			overlaps[0][1], overlaps[0][2], overlaps[0][3], overlaps[0][4])
+		frappe.throw(overlapping_details, title=_('Appointments Overlapping'))
 
 
