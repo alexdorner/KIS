da89ed700094f637a0a77cb9c80a9da772344199
Alexandra Dorner <56341637+alexdorner@users.noreply.github.com>
2021-03-10 21:28:58 +0100

    final change json

--- kis/kis/doctype/patient/patient.py
+++ kis/kis/doctype/patient/patient.py
@@ -3,12 +3,36 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+
+import re #REGULAR EXPRESSION --> library
+import sys
+
 from frappe.model.document import Document
 import frappe
 
 
 class Patient(Document):
-	pass
+
+	def set_email(email):
+		email = frappe.get_doc('Patient ', email)
+		if not re.match("^[a-z]{@,.}*$", email):
+			print("Error! Wrong value! Please insert your email-address")
+			sys.exit
+
+
+
+
+	def set_phone(mobile):
+		mobile = frappe.get_doc('Patient ', mobile)
+		if not re.match("^[0-9]*$", mobile):
+			print
+			"Error! Only Numbers allowed!"
+			sys.exit()
+		elif len(mobile) > 12:
+			print
+			"Error! Just Austrian phone numbers"
+			sys.exit()
+
 
 
 
